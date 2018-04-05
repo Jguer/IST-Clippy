b888b9a39936126bbea63a7cdcce6d3a12cbcce9
Jguer <me@jguer.space>
2018-04-05 21:12:21 +0100

    add arg parsing
    Signed-off-by: Jguer <me@jguer.space>

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+#define N_ARGS 2
 
 int main(int argc, char const *argv[]) {
-    return 0;
+    char *port = NULL;
+    char *ip = NULL;
+
+    if (argc < N_ARGS) {
+        char error[64];
+        perror(sprintf(error, "Usage: %s -c <ip address> <port>\n", argv[0]));
+        return 0;
+    }
 }
