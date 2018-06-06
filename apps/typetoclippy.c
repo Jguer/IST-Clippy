f996a9624845be477fa2d58b0763e4c482e1d820
Jguer <me@jguer.space>
2018-06-06 09:06:14 +0100

    Restore Type to clippy

--- apps/typetoclippy.c
+++ apps/typetoclippy.c
@@ -4,20 +4,63 @@
 
 int main(int argc, char const *argv[]) {
     int clipboard_id = clipboard_connect("./CLIPBOARD_SOCKET");
+    int size, region = 0;
 
     while (1) {
         char buf[4096];
+        printf(
+            "What type of function do you require? P(paste),C(copy) and W(wait)\n");
+
         if (NULL == fgets(buf, 4096, stdin)) {
-            return 1;
+            return -1;
         }
-        buf[strlen(buf) - 1] = '\0';
 
         if (strstr(buf, "exit") != NULL) {
             clipboard_close(clipboard_id);
             return 0;
+        } else if (strstr(buf, "P") != NULL) {
+            printf("From which region do you want to paste?\n");
+            if (scanf("%d", &region) < 0) {
+                printf("Wrong arguments\n");
+                break;
+            }
+            printf("What is the size of what you want to paste\n");
+            if (scanf("%d", &size) < 0) {
+                printf("Wrong arguments\n");
+                break;
+            }
+            clipboard_paste(clipboard_id, region, buf, size);
+            puts(buf);
+        } else if (strstr(buf, "C") != NULL) {
+            printf("Write a message to copied into the clipboard\n");
+            if (NULL == fgets(buf, 4096, stdin)) {
+                break;
+            }
+            printf("To what region do you want to copy?\n");
+            if (scanf("%d", &region) < 0) {
+                printf("Wrong arguments\n");
+                break;
+            }
+            printf("What is the size of the message you want to copy\n");
+            if (scanf("%d", &size) < 0) {
+                printf("Wrong arguments\n");
+                break;
+            }
+            clipboard_copy(clipboard_id, region, buf, size);
+        } else if (strstr(buf, "W") != NULL) {
+            printf("From wich region do you want to paste?\n");
+            if (scanf("%d", &region) < 0) {
+                printf("Wrong arguments\n");
+                break;
+            }
+            printf("What is the size of what you want to paste\n");
+            if (scanf("%d", &size) < 0) {
+                printf("Wrong arguments\n");
+                break;
+            }
+            clipboard_wait(clipboard_id, region, buf, size);
+            puts(buf);
         }
-        clipboard_copy(clipboard_id, 1, buf, strlen(buf) + 1);
-        clipboard_paste(clipboard_id, 1, buf, 4096);
     }
     clipboard_close(clipboard_id);
 }
